3da426a8dbf80fd19e24b955d332bab0e9ad15ca
Zeng Chi-Ruei <s1023030@gmail.com>
2018-04-22 00:28:22 +0800

    fix the problem in input_data.py: to combine heatmaps in the shape [batch_siize,height,width,channels]

--- input_data.py
+++ input_data.py
@@ -13,7 +13,6 @@ def draw_Gaussian_distribution(centerX,centerY,img_w,img_h,amplitude=255,stdx=10
     y=y/(2*stdy)
     combine=(x+y)*(-1)
     final=amplitude*np.exp(combine)
-    #print(final.shape)
     return final
 def next_batch(img_dir_path='',index_path='',img_width=640,img_height=480,batch_size=10,now_at=0):
     X=np.empty([batch_size,img_height,img_width,3])
@@ -24,12 +23,12 @@ def next_batch(img_dir_path='',index_path='',img_width=640,img_height=480,batch_
         img_name=data['filename']
         img=cv2.imread(img_dir_path+img_name)
         X[i]=np.array(img)
+        list_heatmaps=[]
         joints_pos=data['joint_pos']
         for j,joint in zip(range(0,len(joints_pos)),joints_pos):
             x=int(joints_pos[str(joint)][0])
             y=int(joints_pos[str(joint)][1])
-            Y[i,:,:,j]draw_Gaussian_distribution(x,y,img_w=img_width,img_h=img_height))
+            list_heatmaps.append(draw_Gaussian_distribution(x,y,img_w=img_width,img_h=img_height))
+        numpy_heatmaps=np.array(cv2.merge(list_heatmaps[:]))
+        Y[i]=numpy_heatmaps
     return X,Y
-#xx,yy=next_batch(img_dir_path='mpii_human_pose_v1\\output_images\\',
-#index_path='train_data\\new_data.json',batch_size=2,now_at=0)
-#print(xx.shape)
